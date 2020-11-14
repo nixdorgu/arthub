d2bfa5ab53ffedd8e47b1f5a9058be633d2e587b
Anike Nicole Dorgu <naddorgu@gmail.com>
2020-11-14 10:55:08 +0800

    feat: add get routes for / and /register

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const pg = require('pg');
-const dotenv = require('dotenv');
+const express = require("express");
+const expressSession = require("express-session");
+const bodyParser = require("body-parser");
+const pg = require("pg");
+const dotenv = require("dotenv");
 const bcrypt = require("bcrypt");
 
 dotenv.config();
@@ -11,38 +12,43 @@ const port = process.env.PORT ?? 3000;
 const app = express();
 
 app.use(express.json());
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(expressSession({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 pool.connect((error, client) => {
-    if (error){
-        console.log("Could not connect to database.");
-        process.exit(1);
-    }
+  if (error) {
+    console.log("Could not connect to database.");
+    process.exit(1);
+  }
 
-    app.get('/', (req, res) => {});
+  app.get("/", (req, res) => res.sendFile('C:/Users/Acer/Documents/Code/arthub/server/pages/home.html'));
 
-    app.get('/register', (req, res) => {
-        res.send('Register Here!');
+  app.get("/register", (req, res) => {
+    res.sendFile('C:/Users/Acer/Documents/Code/arthub/server/pages/reg.html')
+  });
+
+  app.post("/register", (req, res) => {
+    const { firstName, lastName, email, password } = req.body;
+
+    bcrypt.hash(password, 12, (hashError, hashedPassword) => {
+      if (hashError) return res.status(500).redirect("/register");
+
+      client.query(
+        "INSERT INTO users VALUES (DEFAULT, $1, $2, $3, $4, DEFAULT, DEFAULT) RETURNING *",
+        [firstName, lastName, email, hashedPassword],
+        (dbError, result) => {
+          if (dbError) return res.status(500).redirect("/register");
+          res.redirect('/login');
+        }
+      );
     });
+  });
 
-    app.post('/register', (req, res) => {
-        const {firstName, lastName, email, password} = req.body;
-
-        bcrypt.hash(password, 12, (hashError, hashedPassword) => {
-            if (hashError) return res.status(500).redirect('/register');
-
-            client.query('INSERT INTO users VALUES (DEFAULT, $1, $2, $3, $4, DEFAULT, DEFAULT) RETURNING *', [firstName, lastName, email, hashedPassword], (dbError, result) => {
-                if (dbError) return res.status(500).redirect('/register');
-
-                const user = result.rows[0];
-                res.send(user);
-                // res.redirect('/login');
-            })
-        }); 
-
-    });
-
-    app.listen(port, () => {
+  app.listen(port, () => {
     console.log(`Server is listening on http://localhost:${port}`);
-    });
-})
+  });
+});
