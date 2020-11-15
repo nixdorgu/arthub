d724f74ae3e39fd4ef1bd91cf39a3838e4b401ac
Anike Nicole Dorgu <naddorgu@gmail.com>
2020-11-15 19:42:01 +0800

    refactor: refactor App.js by using components

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,22 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import Card from './components/Card';
 import data from './data';
 
 function App() {
-    const imgSrc = "../../../../../Pictures/2x2.jpg";
-
     return (
         <Router>
             <div className="grid-container">
-            <header className="row">
-                <div>
-                    <a href="/" className="logo">arthub</a>
-                </div>
-                <div>
-                    <a href="#">Artists</a>
-                    <a href="#">Login</a>
-                </div>
-            </header>
-            <main>
-                <div className="row center">
-                    {data.artists.map((artist) => <Card key={artist._id} artist={artist}/>)}
-                </div> 
-            </main>
-            <footer className="row center">
-                <p>All rights reserved &copy; 2020</p>
-            </footer>    
-        </div>
+                <Header/>
+                <main>
+                    <div className="row center">
+                        {data.artists.map((artist) => <Card key={artist._id} artist={artist}/>)}
+                    </div> 
+                </main>
+                <Footer/>
+            </div>
         </Router>
     );
 }
