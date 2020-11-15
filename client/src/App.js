e027cf90d5f83edbd1edd2d7b82d38f0640f3b5e
Anike Nicole Dorgu <naddorgu@gmail.com>
2020-11-15 19:35:12 +0800

    chore: import new project dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Card from './components/Card';
 import data from './data';
 
@@ -6,25 +7,27 @@ function App() {
     const imgSrc = "../../../../../Pictures/2x2.jpg";
 
     return (
-        <div className="grid-container">
-        <header className="row">
-            <div>
-                <a href="/" className="logo">arthub</a>
-            </div>
-            <div>
-                <a href="#">Artists</a>
-                <a href="#">Login</a>
-            </div>
-        </header>
-        <main>
-            <div className="row center">
-                {data.artists.map((artist) => <Card key={artist._id} artist={artist}/>)}
-            </div> 
-        </main>
-        <footer className="row center">
-            <p>All rights reserved &copy; 2020</p>
-        </footer>    
-    </div>
+        <Router>
+            <div className="grid-container">
+            <header className="row">
+                <div>
+                    <a href="/" className="logo">arthub</a>
+                </div>
+                <div>
+                    <a href="#">Artists</a>
+                    <a href="#">Login</a>
+                </div>
+            </header>
+            <main>
+                <div className="row center">
+                    {data.artists.map((artist) => <Card key={artist._id} artist={artist}/>)}
+                </div> 
+            </main>
+            <footer className="row center">
+                <p>All rights reserved &copy; 2020</p>
+            </footer>    
+        </div>
+        </Router>
     );
 }
 
