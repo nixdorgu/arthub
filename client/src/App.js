77f61c988792c5ce2eaee19a905314e9fa18d6a0
Anike Nicole Dorgu <naddorgu@gmail.com>
2020-11-15 19:49:34 +0800

    feat: add preliminary routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Home from './components/Home';
+import Login from './components/Login';
+import Signup from './components/Signup';
 import Card from './components/Card';
+
 import data from './data';
 
 function App() {
@@ -11,6 +16,9 @@ function App() {
             <div className="grid-container">
                 <Header/>
                 <main>
+                    <Route exact path="/" component={Home}></Route>
+                    <Route exact path="/login" component={Login}></Route>
+                    <Route exact path="/register" component={Signup}></Route>
                     <div className="row center">
                         {data.artists.map((artist) => <Card key={artist._id} artist={artist}/>)}
                     </div> 
