675ba99b68a29fc6a999e56cbdb9afd91ad4a594
Anike Nicole Dorgu <naddorgu@gmail.com>
2020-11-15 19:03:05 +0800

    refactor: change image source

--- client/src/data.js
+++ client/src/data.js
@@ -3,56 +3,56 @@ const data = {
         {
             _id: 1,
             name: "Anny Dorgu",
-            image: "../../../../../Pictures/2x2.jpg",
+            image: "images/x-sLGYaQ_stMM-unsplash.jpg",
             rating: 4.5,
             location: "Nigeria"
         },
         {
             _id: 2,
             name: "Suzanna Dorgu",
-            image: "../../../../../Pictures/Unsplash Images/Women Faces/jimmy-fermin-bqe0J0b26RQ-unsplash.jpg",
+            image: "images/jimmy-fermin-bqe0J0b26RQ-unsplash.jpg",
             rating: 4.5,
             location: "Nigeria"
         },
         {
             _id: 3,
             name: "Nala Dorgu",
-            image: "../../../../../Pictures/2x2.jpg",
+            image: "images/x-sLGYaQ_stMM-unsplash.jpg",
             rating: 4.5,
             location: "Nigeria"
         },
         {
             _id: 4,
             name: "Anini Dorgu",
-            image: "../../../../../Pictures/Unsplash Images/Women Faces/jimmy-fermin-bqe0J0b26RQ-unsplash.jpg",
+            image: "images/jimmy-fermin-bqe0J0b26RQ-unsplash.jpg",
             rating: 4.5,
             location: "Nigeria"
         },
         {
             _id: 5,
             name: "Nix Dorgu",
-            image: "../../../../../Pictures/2x2.jpg",
+            image: "images/x-sLGYaQ_stMM-unsplash.jpg",
             rating: 4.5,
             location: "Nigeria"
         },
         {
             _id: 6,
             name: "Nicole Dorgu",
-            image: "../../../../../Pictures/Unsplash Images/Women Faces/jimmy-fermin-bqe0J0b26RQ-unsplash.jpg",
+            image: "images/jimmy-fermin-bqe0J0b26RQ-unsplash.jpg",
             rating: 4.5,
             location: "Nigeria"
         },
         {
             _id: 7,
             name: "Anike Dorgu",
-            image: "../../../../../Pictures/2x2.jpg",
+            image: "images/x-sLGYaQ_stMM-unsplash.jpg",
             rating: 4.5,
             location: "Nigeria"
         },
         {
             _id: 7,
             name: "Anike Dorgu",
-            image: "../../../../../Pictures/Unsplash Images/Women Faces/jimmy-fermin-bqe0J0b26RQ-unsplash.jpg",
+            image: "images/jimmy-fermin-bqe0J0b26RQ-unsplash.jpg",
             rating: 4.5,
             location: "Nigeria"
         }
