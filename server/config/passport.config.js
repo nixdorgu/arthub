01506659ddd42855769a0c01b45d5849d9ca8ecc
Anike Nicole Dorgu <naddorgu@gmail.com>
2020-11-16 12:23:51 +0800

    feat: add passport-jwt strategy

--- server/config/passport.config.js
+++ server/config/passport.config.js
@@ -1,7 +1,10 @@
 const LocalStrategy = require("passport-local").Strategy;
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
 const bcrypt = require("bcrypt");
 
 function initPassport (passport, client) {
+    // local
     const authenticateUser = async(email, password, done) => {      
         const user = await client.query('SELECT * FROM users WHERE email = $1', [email]);
 
@@ -18,9 +21,31 @@ function initPassport (passport, client) {
         })
     }
 
-    passport.use(new LocalStrategy({usernameField: 'email'}, authenticateUser));
-    passport.serializeUser((user, done) => done(null, user));
-    passport.deserializeUser((user, done) => done(null, user))
+    // jwt
+    const options = {
+        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+        secretOrKey: process.env.JWT_SECRET,
+    }
+
+    const jwtAuthenticateUser = (payload, done) => {      
+        return client.query('SELECT * FROM users WHERE user_id = $1', [payload.sub], (err, user) => {
+            if (err) {
+                return done(err, false);
+            }
+
+            if (user) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+            }
+        });
+    }
+
+    const localStrategy = new LocalStrategy({usernameField: 'email'}, authenticateUser)
+    const jwtStrategy = new JwtStrategy(options, jwtAuthenticateUser);
+
+    passport.use(localStrategy);
+    passport.use(jwtStrategy);
 }
 
 module.exports = initPassport;
