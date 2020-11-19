51306af6323d5e578f2569714d4497e597873c6f
Anike Nicole Dorgu <naddorgu@gmail.com>
2020-11-19 12:31:37 +0800

    feat: add migration for creating artist_focus table

---
+++ server/migrations/20201119122828_add_artist_focus_table.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex) {
+  return knex.raw(`
+    CREATE TABLE artist_focus(
+        artist_id INTEGER REFERENCES users(user_id) PRIMARY KEY,
+        focus_id INTEGER REFERENCES focus(id) NOT NULL
+    );
+  `);
+};
+
+exports.down = function(knex) {
+    return knex.raw(`DROP TABLE artist_focus;`);
+};
