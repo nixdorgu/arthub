5902f96fc2afc2fa8d18a53d8190f3fcc935e9bc
Anike Nicole Dorgu <naddorgu@gmail.com>
2020-11-19 13:05:36 +0800

    refactor: change done() return to {} for password-local

--- server/config/passport.config.js
+++ server/config/passport.config.js
@@ -8,15 +8,15 @@ function initPassport (passport, client) {
     const authenticateUser = async(email, password, done) => {      
         const user = await client.query('SELECT * FROM users WHERE email = $1', [email]);
 
-        if (!user) {
-            return done(null, false, {message: 'Incorrect credentials.'})
+        if (user.rows.length === 0) {
+            return done(null, {}, {message: 'Incorrect credentials.'})
         }
 
         bcrypt.compare(password, user.rows[0].password, (error, match) => {
             if (error || !match) {
-                return done (null, false, {message: 'Incorrect credentials'});
+                return done(null, {}, {message: 'Incorrect credentials'});
             }
-            
+
             return done(null, user.rows[0]);
         })
     }
@@ -27,14 +27,14 @@ function initPassport (passport, client) {
         secretOrKey: process.env.JWT_SECRET,
     }
 
-    const jwtAuthenticateUser = (payload, done) => {      
-        return client.query('SELECT * FROM users WHERE user_id = $1', [payload.sub], (err, user) => {
+    const jwtAuthenticateUser = (payload, done) => {   
+        return client.query('SELECT * FROM users WHERE user_id = $1', [payload.user_id], (err, user) => {
             if (err) {
                 return done(err, false);
             }
 
             if (user) {
-                return done(null, user);
+                return done(null, user.rows[0]);
             } else {
                 return done(null, false);
             }
