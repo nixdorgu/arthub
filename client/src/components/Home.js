e8c25f4e84113fbc487d3c6365830cdde0ef2bbd
Anike Nicole Dorgu <naddorgu@gmail.com>
2020-11-22 08:22:56 +0800

    feat: add AJAX call to home component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,38 @@
 import React from 'react';
 
 function Home() {
+  window.onload = () => {
+    const xhr = new XMLHttpRequest();
+    const artists = document.querySelector("#artists");
+
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4) {
+        const response = JSON.parse(xhr.responseText);
+
+        if (xhr.status === 200) {
+          let innerHTML = '';
+
+          for (const artist of response) {
+            innerHTML += `
+              <div key=${artist.user_id}>
+                <h3>${artist.name}</h3>
+                <p>${artist.biography || `Reach me at <a href="mailto:${artist.email}">${artist.email}</a>`}</p>
+              </div>   
+            `;
+          }
+
+          artists.innerHTML = innerHTML;  
+        } else {
+          // Error message
+          // Retry btn? On click refresh page
+        }   
+      }
+    };
+
+    xhr.open("GET", '/api/artists');
+    xhr.send();
+}
+
     return (
         <div style={{display: "flex", flexDirection: "column", alignItems: "center", flexWrap: "wrap"}}>
           <div style={{display: "flex"}}>
