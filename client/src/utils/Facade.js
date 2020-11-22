7fae48f6847dc9bca9c69b6bae64dd6f9b46aef7
Anike Nicole Dorgu <naddorgu@gmail.com>
2020-11-23 07:48:37 +0800

    refactor: correct grammatical errors

--- client/src/utils/Facade.js
+++ client/src/utils/Facade.js
@@ -1,13 +1,13 @@
 export default class Facade {
     
-    post(url, data, sucess, error) {
+    post(url, data, success, error) {
         const xhr = new XMLHttpRequest();
 
         xhr.onreadystatechange = () => {
           if (xhr.readyState === 4) {
             const response = JSON.parse(xhr.responseText);
             
-            if (xhr.status === 200) return sucess(response);
+            if (xhr.status === 200) return success(response);
             else return error(response);
           }
         };
