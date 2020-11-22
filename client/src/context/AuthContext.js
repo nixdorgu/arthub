d0fe97f67fca4dd631326b5f4c7ac83bdbbe17e5
Anike Nicole Dorgu <naddorgu@gmail.com>
2020-11-23 07:36:09 +0800

    feat: encapsulate auth context

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,6 +1,17 @@
-import {createContext} from 'react';
+import {createContext, useState} from 'react';
 
 export const AuthContext = createContext({
     authenticated: false,
     setAuthenticated: null,
 });
+
+
+export function AuthProvider ({children}) {
+    const [authenticated, setAuthenticated] = useState(false);
+
+    return (
+        <AuthContext.Provider value={{authenticated, setAuthenticated}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
