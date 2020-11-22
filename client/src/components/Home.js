10785072850f505daca101ce3330613e25399c9e
Anike Nicole Dorgu <naddorgu@gmail.com>
2020-11-22 21:16:03 +0800

    feat: add function to fetch artists on component mount

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,40 +1,38 @@
-import React from 'react';
+import React, { useContext, useEffect, useState} from 'react';
+import { Redirect } from 'react-router';
+import { AuthContext } from '../AuthContext';
 
 function Home() {
-  window.onload = () => {
+  const ctx = useContext(AuthContext);
+  const [data, setData] = useState([]);
+
+  function fetchArtists() {
     const xhr = new XMLHttpRequest();
-    const artists = document.querySelector("#artists");
 
     xhr.onreadystatechange = () => {
       if (xhr.readyState === 4) {
         const response = JSON.parse(xhr.responseText);
 
         if (xhr.status === 200) {
-          let innerHTML = '';
-
-          for (const artist of response) {
-            innerHTML += `
-              <div key=${artist.user_id}>
-                <h3>${artist.name}</h3>
-                <p>${artist.biography || `Reach me at <a href="mailto:${artist.email}">${artist.email}</a>`}</p>
-              </div>   
-            `;
-          }
-
-          artists.innerHTML = innerHTML;  
+          setData(response);
         } else {
           // Error message
           // Retry btn? On click refresh page
         }   
       }
-    };
+    }
 
     xhr.open("GET", '/api/artists');
     xhr.send();
-}
+  }
+
+  useEffect(() => {
+    fetchArtists()
+  }, []); 
 
     return (
         <div style={{display: "flex", flexDirection: "column", alignItems: "center", flexWrap: "wrap"}}>
+          {ctx.authenticated ? null : <Redirect to="/login"/>}
           <div style={{display: "flex"}}>
               <input type="text" placeholder="Search for artists, genres, etc"></input>
               
