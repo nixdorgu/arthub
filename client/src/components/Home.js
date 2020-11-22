cb170d85ed793999cb664ba5fcee774fba7877bf
Anike Nicole Dorgu <naddorgu@gmail.com>
2020-11-22 21:17:49 +0800

    feat: add boilerplate for rendering artists

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -32,13 +32,20 @@ function Home() {
 
     return (
         <div style={{display: "flex", flexDirection: "column", alignItems: "center", flexWrap: "wrap"}}>
-          {ctx.authenticated ? null : <Redirect to="/login"/>}
+          {/* {ctx.authenticated ? null : <Redirect to="/login"/>} */}
           <div style={{display: "flex"}}>
               <input type="text" placeholder="Search for artists, genres, etc"></input>
               
               <button className="search"><span><i className="fa fa-search" style={{color: "#000"}}></i> Search</span></button>
           </div>
-          <div id="artists"></div>
+          <div id="artists">
+          {data.map((artist) => {
+            return <div key={artist.user_id}>
+                <h3>{artist.name}</h3>
+                <p>{artist.biography || <span>Reach me at <a href={`mailto:${artist.email}`}>{artist.email}</a></span>}</p>
+              </div>   
+            })}
+          </div>
         </div>
     );
 }
