b39cee8c61bb2058c27f19ae12c1d629509115a2
Anike Nicole Dorgu <naddorgu@gmail.com>
2020-11-22 21:31:45 +0800

    feat: add conditional rendering to header component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../AuthContext";
 
 function Header() {
+  const ctx = useContext(AuthContext);
+  // add artists specific navlinks [transactions?]
   return (
     <header className="row">
       <div>
-        <Link to="/" className="logo">arthub</Link>
+        <Link to="/" className="logo">
+          arthub
+        </Link>
       </div>
-      <div>
-        <Link to="/artists">Artists</Link>
-        <Link to="/login">Login</Link>
-      </div>
+      {!ctx.authenticated ? (
+        <div>
+          <Link to="/register">Signup</Link>
+          <Link to="/login">Login</Link>
+        </div>
+      ) : (
+        <div>
+          <Link to="/artists">Profile</Link>
+          <Link to="/login">Messages</Link>
+          <Link to="/login">Random Link Here</Link>
+        </div>
+      )}
     </header>
   );
 }
