2302d05ed33430e425fd8f206128ba0315fcea3a
Anike Nicole Dorgu <naddorgu@gmail.com>
2020-11-22 09:07:26 +0800

    feat: wrap App with auth context provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 import Header from './components/Header';
@@ -6,25 +6,31 @@ import Footer from './components/Footer';
 import Home from './components/Home';
 import Login from './components/Login';
 import Signup from './components/Signup';
+import Profile from './components/Profile';
 import Card from './components/Card';
 
 import data from './data';
+import { AuthContext } from './AuthContext';
 
 function App() {
+    const [authenticated, setAuthenticated] = useState(false);
     return (
         <Router>
-            <div className="grid-container">
-                <Header/>
-                <main>
-                    <Route exact path="/" component={Home}></Route>
-                    <Route exact path="/login" component={Login}></Route>
-                    <Route exact path="/register" component={Signup}></Route>
-                    <div className="row center">
-                        {data.artists.map((artist) => <Card key={artist._id} artist={artist}/>)}
-                    </div> 
-                </main>
-                <Footer/>
-            </div>
+            <AuthContext.Provider value={{authenticated, setAuthenticated}}>
+                <div className="grid-container">
+                    <Header/>
+                    <main>
+                        <Route exact path="/" component={Home}></Route>
+                        <Route exact path="/login" component={Login}></Route>
+                        <Route exact path="/register" component={Signup}></Route>
+                        <Route exact path="/profile" component={() => <Profile props={data.artists[0]}/>}></Route>
+                        {/* <div className="row center">
+                            {data.artists.map((artist) => <Card key={artist._id} artist={artist}/>)}
+                        </div>  */}
+                    </main>
+                    <Footer/>
+                </div>
+            </AuthContext.Provider>
         </Router>
     );
 }
