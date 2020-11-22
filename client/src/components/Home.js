2db73167827f842fdd5994d5ee2e1c997ad51f57
Anike Nicole Dorgu <naddorgu@gmail.com>
2020-11-22 08:22:20 +0800

    feat: add markup to home component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 function Home() {
     return (
-        <div>
-            Home
+        <div style={{display: "flex", flexDirection: "column", alignItems: "center", flexWrap: "wrap"}}>
+          <div style={{display: "flex"}}>
+              <input type="text" placeholder="Search for artists, genres, etc"></input>
+              
+              <button className="search"><span><i className="fa fa-search" style={{color: "#000"}}></i> Search</span></button>
+          </div>
+          <div id="artists"></div>
         </div>
     );
 }
