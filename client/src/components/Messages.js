c6c7376585113f09a995433cd40e35f51d74722e
Anike Nicole Dorgu <naddorgu@gmail.com>
2020-11-28 14:48:45 +0800

    feat: create messages component

---
+++ client/src/components/Messages.js
@@ -0,0 +1,80 @@
+import React, { useCallback, useEffect, useState } from "react";
+import Message from "./Message";
+
+// wala footer dapat diri
+export default function Messages() {
+  const [data, setData] = useState([
+    {
+      user_id: 7,
+      sender_id: 5,
+      content: "I am not okay with this",
+      timestamp: new Date(2020, 10, 20, 15, 30).toLocaleString(),
+    },
+    {
+      user_id: 7,
+      sender_id: 7,
+      content: "How come",
+      timestamp: new Date(2020, 10, 20, 15, 31).toLocaleString(),
+    },
+    {
+      user_id: 7,
+      sender_id: 5,
+      content: "Di ko sure",
+      timestamp: new Date(2020, 10, 20, 15, 55).toLocaleString(),
+    },
+    {
+      user_id: 7,
+      sender_id: 5,
+      content:
+        "Di ko sure kung gaano man gid ko sa kabuhi ko pro oks lang ina, okay?",
+      timestamp: new Date(2020, 10, 20, 15, 55).toLocaleString(),
+    },
+    {
+      user_id: 7,
+      sender_id: 7,
+      content:
+        "Di ko sure kung gaano man gid ko sa kabuhi ko pro oks lang ina, okay?",
+      timestamp: new Date(2020, 10, 20, 15, 55).toLocaleString(),
+    },
+    {
+      user_id: 7,
+      sender_id: 7,
+      content:
+        "Di ko sure kung gaano man gid ko sa kabuhi ko pro oks lang ina, okay?",
+      timestamp: new Date(2020, 10, 20, 15, 55).toLocaleString(),
+    },
+  ]);
+
+  useEffect(() => {}, []);
+
+  // TODO: make scroll down auto
+  return (
+    <div className="messages">
+      {/* <SidePanel /> */}
+      {/* <div> */}
+      {data.map((data, index) => (
+        <Message key={index} props={data} />
+      ))}
+      {/* </div> */}
+      <div className="message-form">
+        <input className="message-input" />
+        <button
+          className="send-message"
+          onClick={() => {
+            setData([
+              ...data,
+              {
+                user_id: 7,
+                sender_id: 7,
+                content: document.querySelector(".message-input").value,
+                timestamp: new Date().toLocaleString(),
+              },
+            ]);
+          }}
+        >
+          Send
+        </button>
+      </div>
+    </div>
+  );
+}
