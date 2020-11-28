bc8957e10cc8d77e0b53727ebc16bb16b400d5d4
Anike Nicole Dorgu <naddorgu@gmail.com>
2020-11-28 14:54:12 +0800

    feat: create function to determine auth state

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,4 +1,6 @@
-import {createContext, useState} from 'react';
+import {createContext, useState, useEffect} from 'react';
+import Facade from '../utils/Facade';
+import {getToken} from '../utils/Tokens';
 
 export const AuthContext = createContext({
     authenticated: false,
@@ -8,7 +10,39 @@ export const AuthContext = createContext({
 
 export function AuthProvider ({children}) {
     const [authenticated, setAuthenticated] = useState(false);
+    const [user, setUser] = useState(null);
 
+    function getAuthState() {
+        const token = getToken();
+
+        if (token) {
+            new Facade()
+            .post(
+                '/api/verify', {token},
+                (response) => {
+                    let user = response.user;
+                    user = {id: user['user_id'], first_name: user['first_name'], last_name: user['last_name'], email: user['email'], user_classification: user['user_classification']}
+
+                    // authenticated
+                    // if res contains token then setToken('arthub_token', response.token)
+                    setAuthenticated(true);
+                    setUser(user);
+                    // alert(response)
+                    console.log(user)
+                },
+                (error) => {
+                    // invalid token ie not authenticated
+                    setAuthenticated(false);
+                }
+            )
+
+        }
+    }
+
+    useEffect(() => {
+        getAuthState();
+    }, []);
+    
     return (
         <AuthContext.Provider value={{authenticated, setAuthenticated}}>
             {children}
