ff10bf8b4fca804276174e8feb767801897b6769
Anike Nicole Dorgu <naddorgu@gmail.com>
2020-11-29 19:38:53 +0800

    feat: add key to message rooms link

--- client/src/components/Messages.js
+++ client/src/components/Messages.js
@@ -1,7 +1,6 @@
 import React, {useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
 import Facade from "../utils/Facade";
-import MessageRoom from "./MessageRoom";
 import {Link} from "react-router-dom";
 
 export default function Messages() {
@@ -24,7 +23,7 @@ export default function Messages() {
   return (
     <div className="messages">
       {rooms.map((data, index) => (
-        <Link to={`/messages/${data.room_id}`}>
+        <Link to={`/messages/${data.room_id}`} key={index}>
           {/* MessageRoom */}
           <div className="room" key={index} style={{width: "100%",background: "#ccc"}}>
             <p>{user.id !== data.user_id ? data.user_name : data.artist_name} <span>{user.id !== data.user_id ? data.user_id : data.artist_id}</span></p>
