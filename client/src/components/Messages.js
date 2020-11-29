50d86a8358a80479254a1749b228158b31081854
Anike Nicole Dorgu <naddorgu@gmail.com>
2020-11-29 15:47:40 +0800

    feat: link messages to message room path

--- client/src/components/Messages.js
+++ client/src/components/Messages.js
@@ -23,6 +23,14 @@ export default function Messages() {
 
   return (
     <div className="messages">
+      {rooms.map((data, index) => (
+        <Link to={`/messages/${data.room_id}`}>
+          {/* MessageRoom */}
+          <div className="room" key={index} style={{width: "100%",background: "#ccc"}}>
+            <p>{user.id !== data.user_id ? data.user_name : data.artist_name} <span>{user.id !== data.user_id ? data.user_id : data.artist_id}</span></p>
+          </div>
+        </Link>
+      ))}
     </div>
   );
 }
