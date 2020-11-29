caa6eec9a00f6c2c689a4b8c5e29aebd5cfe3a59
Anike Nicole Dorgu <naddorgu@gmail.com>
2020-11-29 15:46:32 +0800

    feat: fetch messages on load

--- client/src/components/Messages.js
+++ client/src/components/Messages.js
@@ -1,90 +1,28 @@
 import React, {useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
-import Message from "./Message";
-
-// wala footer dapat diri
-function scrollLastMessageIntoView() {
-  const list = document.querySelectorAll(".message");
-  const element = list[list.length - 1];
-  element.scrollIntoView({ smooth: true });
-}
+import Facade from "../utils/Facade";
+import MessageRoom from "./MessageRoom";
+import {Link} from "react-router-dom";
 
 export default function Messages() {
-  const [data, setData] = useState([
-    {
-      user_id: 7,
-      sender_id: 5,
-      content: "I am not okay with this",
-      timestamp: new Date(2020, 10, 20, 15, 30).toLocaleString(),
-    },
-    {
-      user_id: 7,
-      sender_id: 7,
-      content: "How come",
-      timestamp: new Date(2020, 10, 20, 15, 31).toLocaleString(),
-    },
-    {
-      user_id: 7,
-      sender_id: 5,
-      content: "Di ko sure",
-      timestamp: new Date(2020, 10, 20, 15, 55).toLocaleString(),
-    },
-    {
-      user_id: 7,
-      sender_id: 5,
-      content:
-        "Di ko sure kung gaano man gid ko sa kabuhi ko pro oks lang ina, okay?",
-      timestamp: new Date(2020, 10, 20, 15, 55).toLocaleString(),
-    },
-    {
-      user_id: 7,
-      sender_id: 7,
-      content:
-        "Di ko sure kung gaano man gid ko sa kabuhi ko pro oks lang ina, okay?",
-      timestamp: new Date(2020, 10, 20, 15, 55).toLocaleString(),
-    },
-    {
-      user_id: 7,
-      sender_id: 7,
-      content:
-        "Di ko sure kung gaano man gid ko sa kabuhi ko pro oks lang ina, okay?",
-      timestamp: new Date(2020, 10, 20, 15, 55).toLocaleString(),
-    },
-  ]);
-  const [input, setInput] = useState('');
+  const [rooms, setRooms] = useState([]);
   const user = useContext(AuthContext).user;
 
-  
-  function sendMessage(user, content) {
-    const message = {
-      user_id: user['id'],
-      sender_id: user['id'],
-      content: content,
-      timestamp: new Date().toLocaleString()
-    }
+  useEffect(() => {
+    // add loading indicator
+    const id = user.id;
+    new Facade().get(`/api/messages/${id}`, (response) => {
+      setRooms(response);
+  }, (error) => {
+      // show error page
+      console.log(error)
+  })
 
-    setData([...data, message])
-  }
-
-  useEffect(() => scrollLastMessageIntoView(), [data]);
+    return () => setRooms([])
+  }, [user]); // having rooms here makes it render exponentially
 
   return (
     <div className="messages">
-      {/* <SidePanel /> */}
-      {/* <div> */}
-      {data.map((data, index) => (
-        <Message key={index} props={data} />
-      ))}
-      {/* </div> */}
-      <div className="message-form">
-        <input className="message-input" value={input} onChange={(e) => setInput(e.target.value)}/>
-        <button
-          className="send-message"
-          onClick={() => sendMessage(user, input)}
-        >
-          Send
-        </button>
-      </div>
     </div>
   );
 }
