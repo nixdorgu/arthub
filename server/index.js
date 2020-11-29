185eca072e6d62c38fbd53bd56c55bb0d26f94c5
Anike Nicole Dorgu <naddorgu@gmail.com>
2020-11-29 12:28:22 +0800

    style: format index.js

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const pg = require("pg");
 const dotenv = require("dotenv");
 const bcrypt = require("bcrypt");
 const passport = require("passport");
 const jwt = require("jsonwebtoken");
-const http = require('http');
+const http = require("http");
 
 const initPassport = require("./config/passport.config");
 const checkConfig = require("./config/envError");
-const formatPayload = require('./src/formatPayload');
+const formatPayload = require("./src/formatPayload");
 
 dotenv.config();
 
@@ -20,126 +20,170 @@ const app = express();
 const server = http.createServer(app);
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(passport.initialize());
 
 pool.connect((error, client) => {
   checkConfig(error);
-  
+
   // protected - not logged in
-  app.post("/api/login", passport.authenticate('local', {session: false}), (req, res, next) => {
-    // try console req and see if error message shows up
-    let user = req.user;
+  app.post(
+    "/api/login",
+    passport.authenticate("local", { session: false }),
+    (req, res, next) => {
+      // try console req and see if error message shows up
+      let user = req.user;
 
-    if (JSON.stringify(user) === JSON.stringify({})) return res.status(401).json({success: false, message: "Incorrect credentials."})
+      if (JSON.stringify(user) === JSON.stringify({}))
+        return res
+          .status(401)
+          .json({ success: false, message: "Incorrect credentials." });
 
-    user = formatPayload(user);
+      user = formatPayload(user);
 
-    return req.login(user, {session: false}, (error) => {
-      if (error) {
-        return res.status(500).json({success: false, message: "Something went wrong"});
-      } else {
-        return jwt.sign(user, process.env.JWT_SECRET, {expiresIn: '1d'}, (err, token) => {
-          if (err) return res.status(500).json({success: false, message: "Something went wrong"});
-          return res.status(200).json({token});
-        })
-      }
-    })
-  });
+      return req.login(user, { session: false }, (error) => {
+        if (error) {
+          return res
+            .status(500)
+            .json({ success: false, message: "Something went wrong" });
+        } else {
+          return jwt.sign(
+            user,
+            process.env.JWT_SECRET,
+            { expiresIn: "1d" },
+            (err, token) => {
+              if (err)
+                return res
+                  .status(500)
+                  .json({ success: false, message: "Something went wrong" });
+              return res.status(200).json({ token });
+            }
+          );
+        }
+      });
+    }
+  );
 
   // protected - not logged in
   app.post("/api/register", (req, res) => {
     const { firstName, lastName, email, password, isArtist } = req.body;
 
     bcrypt.hash(password, 12, (hashError, hashedPassword) => {
-      if (hashError) return res.status(500).json({success: "false", message: "Something went wrong."});
+      if (hashError)
+        return res
+          .status(500)
+          .json({ success: "false", message: "Something went wrong." });
 
-      client.query('SELECT email FROM users WHERE email = $1', [email], (error, duplicate) => {
-        if (error || duplicate.rows.length != 0) {
-          return res.status(409).json({success: "false", message: "Email already in use."})
-        } else {
-          const userClassification = isArtist ? "artist": "customer";
+      client.query(
+        "SELECT email FROM users WHERE email = $1",
+        [email],
+        (error, duplicate) => {
+          if (error || duplicate.rows.length != 0) {
+            return res
+              .status(409)
+              .json({ success: "false", message: "Email already in use." });
+          } else {
+            const userClassification = isArtist ? "artist" : "customer";
 
-          const onSuccessfulRegistration = (res, user) => {
-            const name = user.first_name + ' ' + user.last_name;
+            const onSuccessfulRegistration = (res, user) => {
+              const name = user.first_name + " " + user.last_name;
 
-            Object.assign(user, {name});
+              Object.assign(user, { name });
 
-            Reflect.deleteProperty(user, "user_id");
-            Reflect.deleteProperty(user, "first_name");
-            Reflect.deleteProperty(user, "last_name");
-            Reflect.deleteProperty(user, "password");
-            Reflect.deleteProperty(user, "status");
-            Reflect.deleteProperty(user, "user_classification");
-            
-            return res.status(200).json({success: true, user});
+              Reflect.deleteProperty(user, "user_id");
+              Reflect.deleteProperty(user, "first_name");
+              Reflect.deleteProperty(user, "last_name");
+              Reflect.deleteProperty(user, "password");
+              Reflect.deleteProperty(user, "status");
+              Reflect.deleteProperty(user, "user_classification");
+
+              return res.status(200).json({ success: true, user });
+            };
+
+            const createReferenceToArtist = (res, user) => {
+              const id = user.user_id;
+              return client.query(
+                "INSERT INTO artists (artist_id) VALUES ($1)",
+                [id],
+                (error, result) => {
+                  if (error) return res.status(500).json({ success: false });
+                  return onSuccessfulRegistration(res, user);
+                }
+              );
+            };
+
+            const onError = (res, dbError, result) => {
+              if (dbError)
+                return res
+                  .status(500)
+                  .json({ success: false, message: "Something went wrong." });
+
+              const user = result.rows[0];
+              return user.user_classification !== "artist"
+                ? onSuccessfulRegistration(res, user)
+                : createReferenceToArtist(res, user);
+            };
+
+            client.query(
+              "INSERT INTO users VALUES (DEFAULT, $1, $2, $3, $4, DEFAULT, DEFAULT, $5) RETURNING *",
+              [firstName, lastName, email, hashedPassword, userClassification],
+              (dbError, result) => onError(res, dbError, result)
+            );
           }
-
-          const createReferenceToArtist = (res, user) => {
-            const id = user.user_id;
-            return client.query('INSERT INTO artists (artist_id) VALUES ($1)', [id], (error, result) => {
-              if (error) return res.status(500).json({success: false});
-              return onSuccessfulRegistration(res, user);
-            });
-          }
-
-          const onError = (res, dbError, result) => {
-            if (dbError) return res.status(500).json({success: false, message: "Something went wrong."});
-
-            const user = result.rows[0];
-            return user.user_classification !== 'artist' ? onSuccessfulRegistration(res, user): createReferenceToArtist(res, user);
-          }
-
-          client.query(
-            "INSERT INTO users VALUES (DEFAULT, $1, $2, $3, $4, DEFAULT, DEFAULT, $5) RETURNING *",
-            [firstName, lastName, email, hashedPassword, userClassification],
-            (dbError, result) => onError(res, dbError, result)
-          );
         }
-      });
-    })
+      );
+    });
   });
 
   // protected
-  app.get('api/profile', (req, res) => {
-    res.send('Profile')
+  app.get("api/profile", (req, res) => {
+    res.send("Profile");
   });
 
   // protected
-  app.get('/api/artists', (req, res) => {
-    client.query("SELECT user_id, CONCAT(first_name, ' ', last_name) AS name, email, biography FROM users INNER JOIN artists ON user_id = artist_id", (error, result) => {
-      if (error) return res.status(500).json({success: false, message: "Something went wrong."});
+  app.get("/api/artists", (req, res) => {
+    client.query(
+      "SELECT user_id, CONCAT(first_name, ' ', last_name) AS name, email, biography FROM users INNER JOIN artists ON user_id = artist_id",
+      (error, result) => {
+        if (error)
+          return res
+            .status(500)
+            .json({ success: false, message: "Something went wrong." });
 
-      return res.status(200).json(result.rows);
-    })
+        return res.status(200).json(result.rows);
+      }
+    );
   });
 
-  app.get('/api/artists/:id', (req, res) => {});
+  app.get("/api/artists/:id", (req, res) => {});
 
   // protected
-  app.get('/api/transactions', (req, res) => {});
+  app.get("/api/transactions", (req, res) => {});
 
   // protected
-  app.get('/api/transactions/:id', (req, res) => {});
-  
-  // protected
-  app.post('/api/logout', (req, res) => {})
+  app.get("/api/transactions/:id", (req, res) => {});
 
-  app.post('/api/verify', (req, res) => {
-    const {token} = req.body;
+  // protected
+  app.post("/api/logout", (req, res) => {});
+
+  app.post("/api/verify", (req, res) => {
+    const { token } = req.body;
 
     jwt.verify(token, process.env.JWT_SECRET, {}, (error, authentic) => {
-      if (error) return res.status(403).json({success: false, message: "Invalid token."})
+      if (error)
+        return res
+          .status(403)
+          .json({ success: false, message: "Invalid token." });
 
       if (authentic.exp < Date.now()) {
         // res.json({success: false, message: "Expired token."})
         // refresh token here
       }
 
-      res.status(200).json({success: true, user: authentic});
+      res.status(200).json({ success: true, user: authentic });
     });
-  })
+  });
 
   server.listen(port, () => {
     initPassport(passport, client);
