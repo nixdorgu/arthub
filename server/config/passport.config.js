b9e9f475a573f2c29dbcbe1bebde5652daceb6c9
Anike Nicole Dorgu <naddorgu@gmail.com>
2020-11-30 15:12:49 +0800

    refactor: remove passport-jwt

--- server/config/passport.config.js
+++ server/config/passport.config.js
@@ -1,6 +1,4 @@
 const LocalStrategy = require("passport-local").Strategy;
-const JwtStrategy = require("passport-jwt").Strategy;
-const ExtractJwt = require("passport-jwt").ExtractJwt;
 const bcrypt = require("bcrypt");
 
 function initPassport (passport, client) {
@@ -23,31 +21,8 @@ function initPassport (passport, client) {
         })
     }
 
-    // jwt
-    const options = {
-        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-        secretOrKey: process.env.JWT_SECRET,
-    }
-
-    const jwtAuthenticateUser = (payload, done) => {   
-        return client.query('SELECT * FROM users WHERE user_id = $1', [payload.user_id], (err, user) => {
-            if (err) {
-                return done(err, false);
-            }
-
-            if (user) {
-                return done(null, user.rows[0]);
-            } else {
-                return done(null, false);
-            }
-        });
-    }
-
     const localStrategy = new LocalStrategy({usernameField: 'email'}, authenticateUser)
-    const jwtStrategy = new JwtStrategy(options, jwtAuthenticateUser);
-
     passport.use(localStrategy);
-    passport.use(jwtStrategy);
 }
 
 module.exports = initPassport;
