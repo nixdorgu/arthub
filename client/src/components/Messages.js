c4ed865fd12d410678258ce49633fe9ef9f39268
Anike Nicole Dorgu <naddorgu@gmail.com>
2020-11-30 16:19:59 +0800

    refactor: modularize code for fetching messages

--- client/src/components/Messages.js
+++ client/src/components/Messages.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState } from "react";
+import React, {useCallback, useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
 import Facade from "../utils/Facade";
 import {Link} from "react-router-dom";
@@ -9,15 +9,20 @@ export default function Messages() {
   const [rooms, setRooms] = useState([]);
   const user = useContext(AuthContext).user;
 
+  function getRooms(id) {
+    return new Facade().get(`/api/messages/${id}`,
+    (response) => {
+      setRooms(response);
+      setTimeout(() => getRooms(id), 3000);
+    }, (error) => {
+        // show error page
+        console.log(error)
+    })
+  }
   useEffect(() => {
     // add loading indicator
     const id = user.id;
-    new Facade().get(`/api/messages/${id}`, (response) => {
-      setRooms(response);
-  }, (error) => {
-      // show error page
-      console.log(error)
-  })
+    getRooms(id);
 
     return () => setRooms([])
   }, [user]); // having rooms here makes it render exponentially
