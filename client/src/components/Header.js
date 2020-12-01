b8d0176627cdfac55b1240b2864dd440932f27eb
Anike Nicole Dorgu <naddorgu@gmail.com>
2020-12-01 16:40:53 +0800

    feat: add hamburger menu

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,22 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import "./header.css";
 
 function Header() {
   const ctx = useContext(AuthContext);
+  const [hamburgerOpen, setHamburgerOpen] = useState(false);
+
+  function showLinks() {
+    const navlinks = document.querySelector('.hide-mobile');
+    navlinks.classList.toggle('show', !hamburgerOpen);
+
+    const children = navlinks.firstChild.childNodes;
+    children.forEach(child => child.classList.toggle('active'))
+  }
+  
   // add artists specific navlinks [transactions?]
   return (
     <header className="row">
@@ -12,18 +25,40 @@ function Header() {
           arthub
         </Link>
       </div>
-      {!ctx.authenticated ? (
-        <div>
-          <Link to="/register">Signup</Link>
-          <Link to="/login">Login</Link>
-        </div>
-      ) : (
-        <div>
-          <Link to="/profile">Profile</Link>
-          <Link to="/messages">Messages</Link>
-          <Link to="/login">Random Link Here</Link>
-        </div>
-      )}
+
+      <div className="hide-mobile">
+        {!ctx.authenticated ? (
+          <div>
+            <Link to="/register">Signup</Link>
+            <Link to="/login">Login</Link>
+          </div>
+        ) : (
+          <div>
+            <Link to="/profile">Profile</Link>
+            <Link to="/messages">Messages</Link>
+            <Link to="/login">Random Link Here</Link>
+          </div>
+        )}
+      </div>
+      <div className="hamburger show-mobile">
+        {hamburgerOpen ? (
+          <FontAwesomeIcon
+            icon={faTimes}
+            onClick={(e) => {
+              showLinks()
+              setHamburgerOpen(!hamburgerOpen)}
+            }
+          />
+        ) : (
+          <FontAwesomeIcon
+            icon={faBars}
+            onClick={(e) => {
+              showLinks()
+              setHamburgerOpen(!hamburgerOpen)}
+            }
+          />
+        )}
+      </div>
     </header>
   );
 }
