a728596f89e314e61baad22903ed97b7bed51291
Anike Nicole Dorgu <naddorgu@gmail.com>
2020-12-03 18:07:16 +0800

    feat: add route for search bar on client-side

--- server/index.js
+++ server/index.js
@@ -157,7 +157,19 @@ pool.connect((error, client) => {
     );
   });
 
-  app.get("/api/artists/:id", (req, res) => {});
+  app.get("/api/artists/:name", (req, res) => {
+    client.query(
+      `SELECT user_id, CONCAT(first_name, ' ', last_name) AS name, email, biography FROM users INNER JOIN artists ON user_id = artist_id INNER JOIN artist_focus USING (artist_id) INNER JOIN focus USING (focus_id) WHERE CONCAT(first_name, ' ', last_name) ILIKE '%${req.params.name}%'`,
+      (error, result) => {
+        if (error)
+          return res
+            .status(500)
+            .json({ success: false, message: "Something went wrong." });
+
+        return res.status(200).json(result.rows);
+      }
+    );
+  });
 
   // protected
   app.get("/api/messages/:id", (req, res) => {
