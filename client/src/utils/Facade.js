cdb7a68525cd9492ba27e5b0039968c364128833
Anike Nicole Dorgu <naddorgu@gmail.com>
2020-12-06 22:29:07 +0800

    refactor: change authentication header to authorization header

--- client/src/utils/Facade.js
+++ client/src/utils/Facade.js
@@ -2,24 +2,24 @@ import { getToken } from "./Tokens";
 
 export default class Facade {
     post(url, data, success, error) {
-        const xhr = new XMLHttpRequest();
-        const token = getToken();
+      const xhr = new XMLHttpRequest();
+      const token = getToken();
 
 
-        xhr.onreadystatechange = () => {
-          if (xhr.readyState === 4) {
-            const response = JSON.parse(xhr.responseText);
-            
-            if (xhr.status === 200) return success(response);
-            else return error(response);
-          }
-        };
-    
-        
-        xhr.open("POST", url);
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.setRequestHeader("Authentication", `Bearer ${token}`);
-        xhr.send(JSON.stringify(data));
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+          const response = JSON.parse(xhr.responseText);
+          
+          if (xhr.status === 200) return success(response);
+          else return error(response);
+        }
+      };
+  
+      
+      xhr.open("POST", url);
+      xhr.setRequestHeader("Content-Type", "application/json");
+      xhr.setRequestHeader("Authorization", `Bearer ${token}`);
+      xhr.send(JSON.stringify(data));
     }
 
     get(url, success, error) {
@@ -37,7 +37,7 @@ export default class Facade {
       };
   
       xhr.open("GET", url);
-      xhr.setRequestHeader("Authentication", `Bearer ${token}`);
+      xhr.setRequestHeader("Authorization", `Bearer ${token}`);
       xhr.send();
     }
 }
