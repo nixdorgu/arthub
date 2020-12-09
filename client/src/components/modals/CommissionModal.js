6303a74cec5279974eadbae036fe0e2c0bd35121
Anike Nicole Dorgu <naddorgu@gmail.com>
2020-12-09 18:54:29 +0800

    feat: add price input and add aria attributes

--- client/src/components/modals/CommissionModal.js
+++ client/src/components/modals/CommissionModal.js
@@ -4,9 +4,11 @@ import Modal from './Modal';
 export default function CommissionModal({show, handleClose, handleSubmit}) {
     return (
         <Modal show={show} handleClose={handleClose} header={'Commission Form'} handleSubmit={handleSubmit}>
-                    <input type="text" placeholder="Title (maximum of 30 characters)" min="1" max="30" required/>
-                    <textarea className="short-description" placeholder="Short description (maximum of 80 characters)" min="1" max="80" required/>
-                    <textarea className="long-description" placeholder="Long description (maximum of 300 characters)" min="1" max="300" required/>
+            <input type="text" aria-label="title" placeholder="Title (maximum of 30 characters)" min="1" max="30" required/>
+            <textarea className="short-description" aria-label="short description" placeholder="Short description (maximum of 80 characters)" min="1" max="80" required/>
+            <textarea className="long-description" aria-label="long description" placeholder="Long description (maximum of 300 characters)" min="1" max="300" required/>
+            <input id="price" name="price" type="number" min="1.00" step="any" aria-label="proposed price" placeholder="Proposed price"/>
+            <p className="price-disclaimer">* We operate using USD</p>
         </Modal>
     )
 }
