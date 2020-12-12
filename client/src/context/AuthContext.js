ed6be35d56387b1b75870133e9a22a61bd6c8485
Anike Nicole Dorgu <naddorgu@gmail.com>
2020-12-12 20:50:33 +0800

    refactor: refactor auth context

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,19 +1,16 @@
-import {createContext, useState, useEffect} from 'react';
+import {createContext, useState, useEffect, useContext} from 'react';
 import Facade from '../utils/Facade';
 import {getToken} from '../utils/Tokens';
 
-export const AuthContext = createContext({
-    authenticated: false,
-    user: {},
-    setAuthenticated: null,
-});
+export const AuthContext = createContext();
 
+export const useAuth = () => useContext(AuthContext);
 
 export function AuthProvider ({children}) {
-    const [authenticated, setAuthenticated] = useState(false);
+    const [authenticated, setAuthenticated] = useState(true);
     const [user, setUser] = useState({});
 
-    function getAuthState() {
+    useEffect(() => {
         const token = getToken();
 
         if (token) {
@@ -21,27 +18,27 @@ export function AuthProvider ({children}) {
             .post(
                 '/api/verify', {token},
                 (response) => {
+                    setAuthenticated(true);
+
                     let user = response.user;
                     user = {id: user['user_id'], first_name: user['first_name'], last_name: user['last_name'], email: user['email'], user_classification: user['user_classification']}
 
                     // authenticated
                     // if res contains token then setToken('arthub_token', response.token)
-                    setAuthenticated(true);
                     setUser(user);
                     // alert(response)
                     console.log(user)
                 },
                 (error) => {
+                    console.log(error)
                     // invalid token ie not authenticated
                     setAuthenticated(false);
                 }
             )
 
+        } else {
+            setAuthenticated(false);
         }
-    }
-
-    useEffect(() => {
-        getAuthState();
     }, [authenticated]);
     
     return (
