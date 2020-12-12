1f4ae51021825b2552a3ff7779601648bf021267
Anike Nicole Dorgu <naddorgu@gmail.com>
2020-12-13 02:23:21 +0800

    refactor: refactor artists/name database query

--- server/index.js
+++ server/index.js
@@ -193,7 +193,7 @@ pool.connect((error, client) => {
   app.get("/api/artists/:name", (req, res) => {
     return client.query(
       // `SELECT user_id, CONCAT(first_name, ' ', last_name) AS name, email, biography FROM users INNER JOIN artists ON user_id = artist_id INNER JOIN artist_focus USING (artist_id) INNER JOIN focus USING (focus_id) WHERE CONCAT(first_name, ' ', last_name) ILIKE '%${req.params.name}%'`,
-      `SELECT user_id, CONCAT(first_name, ' ', last_name) AS name, email, biography FROM users INNER JOIN artists ON user_id = artist_id WHERE CONCAT(first_name, ' ', last_name) ILIKE '%${req.params.name}%'`,
+      `SELECT user_id, CONCAT(first_name, ' ', last_name) AS name, email, biography FROM users INNER JOIN artists ON user_id = artist_id WHERE CONCAT(first_name, ' ', last_name) ILIKE $1`, [`%${req.params.name}%`],
       (error, result) => {
         if (error)
           return res
