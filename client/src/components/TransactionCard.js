4dc972d40a77a5a0a94701a31814996415762366
Anike Nicole Dorgu <naddorgu@gmail.com>
2020-12-12 20:52:07 +0800

    feat: add preliminary logic to transaction card component

--- client/src/components/TransactionCard.js
+++ client/src/components/TransactionCard.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
+import Facade from "../utils/Facade";
+import PendingTransactionModal from "./modals/PendingTransactionModal";
 
 export default function TransactionCard(props) {
   const { transaction, user } = props.props;
+  const [showModal, setShowModal] = useState(false);
+  const isArtist = transaction.artist_id === user.id; // wrap into fn + status stuff
+  const status = transaction.status;
+
+  const submit = (e, transaction) => {
+    setShowModal(false);
+    e.preventDefault();
+
+    new Facade().patch(`/api/transactions/${transaction.transaction_id}`, {classification: isArtist? 'payment pending' : 'cancelled'}, (success) => {
+      console.log(JSON.stringify(success))
+
+    }, (error) => console.log(JSON.stringify(error)))
   
+    
+  }
+
   return (
+    <>
+    {status === "pending" && showModal && <PendingTransactionModal isArtist={isArtist} transaction={transaction} show={showModal} handleClose={(e) => setShowModal(false)} handleSubmit={(e) => submit(e, transaction)} />}
+    {status === "payment pending" && !isArtist && showModal && <PendingTransactionModal isArtist={isArtist} transaction={transaction} show={showModal} handleClose={(e) => setShowModal(false)} handleSubmit={(e) => submit(e, transaction)} />}
     <div
       key={transaction.transaction_id}
       style={{
         boxShadow: "1px 0px 5px 2px #ccc",
         padding: "1rem",
+        marginBottom: "1rem"
+      }}
+      onClick={(e) => {
+        if (!e.target.classList?.contains("transaction-link")) {
+          setShowModal(true)
+        }
       }}
     >
       <h4>{transaction.title}</h4>
@@ -21,11 +47,11 @@ export default function TransactionCard(props) {
         }}
       >
         {transaction.artist_id !== user.id ? (
-          <a href={`profile/${transaction.artist_id}`}>
+          <a className="transaction-link" href={`profile/${transaction.artist_id}`}>
             Commissioned to {transaction.artist_name}
           </a>
         ) : (
-          <a href={`profile/${transaction.user_id}`}>
+          <a className="transaction-link" href={`profile/${transaction.user_id}`}>
             Commissioned by {transaction.customer_name}
           </a>
         )}
@@ -54,5 +80,6 @@ export default function TransactionCard(props) {
         <p style={{ fontSize: ".7rem" }}>{transaction.status}</p>
       </div>
     </div>
+    </>
   );
 }
