4c1a77190eac2a9f25fdf9e5144951ce3a82c345
Anike Nicole Dorgu <naddorgu@gmail.com>
2020-12-12 14:04:16 +0800

    fix: fix logical error when rendering unprotected routes

--- client/src/components/UnprotectedRoute.js
+++ client/src/components/UnprotectedRoute.js
@@ -2,12 +2,12 @@ import React, { useContext } from 'react'
 import { Redirect, Route } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext'
 
-export default function ProtectedRoute({component: Component, path, ...rest}) {
+export default function UnprotectedRoute({component: Component, path, ...rest}) {
     const auth = useContext(AuthContext);
 
     return (
         <Route path={path} {...rest} render={props => {
-            return auth.authenticated ? (
+            return !auth.authenticated ? (
                 <Component {...props}/>
             ) : (
                 <Redirect to="/"/>
