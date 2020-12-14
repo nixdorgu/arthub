14a9dfb76f093954c3d1d9c95e887fa19d7093d5
Anike Nicole Dorgu <naddorgu@gmail.com>
2020-12-14 20:48:22 +0800

    feat: add facebook login strategy

--- server/config/passport.config.js
+++ server/config/passport.config.js
@@ -1,5 +1,7 @@
 const LocalStrategy = require("passport-local").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
 const bcrypt = require("bcrypt");
+require('dotenv').config();
 
 function initPassport (passport, client) {
     // local
@@ -21,8 +23,27 @@ function initPassport (passport, client) {
         })
     }
 
+    const authenticateFacebookUser = async(accessToken, refreshToken, profile, done) => {
+        const {first_name, last_name, email} = profile._json;
+
+        const user = await client.query('SELECT * FROM users WHERE email = $1', [email]);
+
+        if (user.rows.length === 0) {
+            client.query(`INSERT INTO users (user_id, email, first_name, last_name, member_since, status, user_classification) VALUES (DEFAULT, $1, $2, $3, DEFAULT, DEFAULT, DEFAULT) RETURNING *`, [ email, first_name, last_name], (error, result) => {
+                if (error) return done(null, {}, {message: 'Account not found.'});
+
+                return done(null, result.rows[0])
+            })
+        } else {
+            return (user.rows[0]['status'] !== 'active') ? done(null, {}, {message: 'Account suspended'}) : done(null, user.rows[0])
+        }  
+    };
+
     const localStrategy = new LocalStrategy({usernameField: 'email'}, authenticateUser)
+    const facebookStrategy = new FacebookStrategy({clientID: process.env.FB_APP_ID, clientSecret: process.env.FB_APP_SECRET, callbackURL: "http://localhost:5000/auth/facebook/callback", profileFields: ['id', 'email', 'name']}, authenticateFacebookUser)
+
     passport.use(localStrategy);
+    passport.use(facebookStrategy);
 }
 
 module.exports = initPassport;
