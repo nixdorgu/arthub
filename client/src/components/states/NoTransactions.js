a85ff5ca73ad06c5352b2d71dcad362d9ef960ee
Anike Nicole Dorgu <naddorgu@gmail.com>
2020-12-17 02:39:14 +0800

    feat: add image and text to no transactions component

--- client/src/components/states/NoTransactions.js
+++ client/src/components/states/NoTransactions.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import plant from "../../images/plant.svg";
 
 export default function NoTransactions() {
-    return (
-        <div style={{height: "100vh", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
-            No orders yet!
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "space-evenly",
+        height: "calc(100vh - 5rem"
+      }}
+    >
+        <div>
+        <h1 style={{textAlign: "center"}}>No Transactions</h1>
+{/* <button className="button">Hi</button> */}
         </div>
-    )
+      <img
+        src={plant}
+        alt="Serenity"
+        style={{
+          width: "100%",
+          display: "block",
+          maxHeight: "50vh",
+          margin: "2rem 0",
+        }}
+      />
+      <i style={{ textAlign: "center" }}>
+        "Art will nurture your soul just as music does."
+      </i>
+    </div>
+  );
 }
