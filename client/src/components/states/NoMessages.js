c443d461fa106e3597fe9e5efcadb3a542e40451
Anike Nicole Dorgu <naddorgu@gmail.com>
2020-12-17 02:16:59 +0800

    feat: add images and text to NoMessages states component

--- client/src/components/states/NoMessages.js
+++ client/src/components/states/NoMessages.js
@@ -1,13 +1,28 @@
-import React from 'react'
+import React from "react";
+
+import laying from "../../images/laying.svg";
 
 export default function NoMessages() {
-    return (
-        <div style={{height: "100vh", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
-            No messages yet!
-        </div>
-    )
+  return (
+    <div
+      style={{
+        height: "calc(100vh - 5rem)",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <h1>No messages yet</h1>
+      <img
+        src={laying}
+        alt="Serenity"
+        style={{ width: "100%", display: "block", maxHeight: "50vh", margin: "2rem 0" }}
+      />
+      <i style={{textAlign: "center"}}>
+        "Let the conversation flow at its own pace. Don’t try to rush it or
+        control it. You need to let go and be part of the conversation."
+      </i>
+    </div>
+  );
 }
-
-
-
-
