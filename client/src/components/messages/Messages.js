21442fe9a4d3a7995663e14155744b65256cdc08
Anike Nicole Dorgu <naddorgu@gmail.com>
2020-12-18 20:58:49 +0800

    feat: change displays, add error state, and timeout

--- client/src/components/messages/Messages.js
+++ client/src/components/messages/Messages.js
@@ -1,49 +1,72 @@
-import React, {useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState, useCallback } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import Facade from "../../utils/Facade";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import NoMessages from "../states/NoMessages";
 import showRecipient from "../../tests/showRecipient";
+import LoadingIndicator from "../LoadingIndicator";
 
 export default function Messages() {
+  let timeout;
   const [rooms, setRooms] = useState([]);
+  const [error, setError] = useState(false);
   const user = useContext(AuthContext).user;
 
-  function getRooms(id) {
-    return new Facade().get(`/api/messages/${id}`,
-    (response) => {
-      setRooms(response);
-      setTimeout(() => getRooms(id), 3000);
-    }, (error) => {
+  const getRooms = useCallback(() => {
+    new Facade().get(
+      `/api/messages/${user.id}`,
+      (response) => {
+        setError(false);
+        setRooms(response);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        timeout = setTimeout(() => getRooms(user.id), 10000);
+      },
+      (error) => {
         // show error page
-        console.log(error)
-    })
-  }
+        setError(true);
+        console.log(error);
+      }
+    );
+  }, [user]);
+
   useEffect(() => {
     // add loading indicator
-    const id = user.id;
-    getRooms(id);
-
-    return () => setRooms([])
-  }, [user]); // having rooms here makes it render exponentially
+    getRooms();
+    return () => clearTimeout(timeout);
+  }, [getRooms, timeout]); // having rooms here makes it render exponentially
 
   return (
     <div className="messages">
       {
         rooms.length > 0 ? (
           rooms.map((data, index) => (
-            <Link to={`/messages/${data.room_id}`} key={index} className="room-link">
+            <Link
+              to={`/messages/${data.room_id}`}
+              key={index}
+              className="room-link"
+            >
               {/* MessageRoom */}
-              <div className="room" key={index} style={{width: "100%",background: "#ccc"}}>
+              <div
+                className="room"
+                key={index}
+                style={{ width: "100%", background: "#ccc" }}
+              >
                 <p className="room-recipient">{showRecipient(data, user.id)}</p>
                 <div className="room-content">
                   <p className="room-message">{data.last_message}</p>
-                  <p className="room-timestamp">{new Date(data.sent_at).toLocaleString()}</p>
+                  <p className="room-timestamp">
+                    {new Date(data.sent_at).toLocaleString()}
+                  </p>
                 </div>
               </div>
             </Link>
           ))
-        ) : (<NoMessages/>)
+        ) : error ? (
+          <NoMessages />
+        ) : (
+          <LoadingIndicator />
+        )
+        // loading indicator if error i false else nomes
       }
     </div>
   );
