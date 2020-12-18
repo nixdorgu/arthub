0da7895c3b8e329b94f77a89b995abddbcfc7e38
Anike Nicole Dorgu <naddorgu@gmail.com>
2020-12-18 20:43:28 +0800

    fix: fix scrolling behaviout of component

--- client/src/components/messages/MessageRoom.js
+++ client/src/components/messages/MessageRoom.js
@@ -1,17 +1,12 @@
-import React, {useCallback, useContext, useEffect, useState } from "react";
+import React, {useCallback, useContext, useEffect, useState, useRef } from "react";
 import {Redirect, useParams} from 'react-router-dom';
 import { AuthContext } from "../../context/AuthContext";
 import Facade from "../../utils/Facade";
 import LoadingIndicator from "../LoadingIndicator";
 import Message from "./Message";
 
-function scrollLastMessageIntoView() {
-  const list = document.querySelectorAll(".message");
-  const element = list[list.length - 1];
-
-  if (element) {
-    element.scrollIntoView({ smooth: true });
-  }
+function scrollLastMessageIntoView(inputRef) {
+  inputRef.current.scrollIntoView({ smooth: true });
 }
 
 export default function MessageRoom() {
@@ -21,6 +16,7 @@ export default function MessageRoom() {
   const [input, setInput] = useState('');
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
+  const inputRef = useRef();
 
   const user = useContext(AuthContext).user;
 
@@ -29,8 +25,9 @@ export default function MessageRoom() {
       setData(response);
       setLoading(false);
       // eslint-disable-next-line react-hooks/exhaustive-deps
-      timeout = setTimeout(() => fetchMessages(room), 5000); // end-to-end communication with less requests per min than adding dep
+      timeout = setTimeout(() => fetchMessages(room), 9000); // end-to-end communication with less requests per min than adding dep
       console.log(response)
+      scrollLastMessageIntoView(inputRef)
     }, (error) => {
       // show message
       console.log(error)
@@ -47,24 +44,17 @@ export default function MessageRoom() {
     }
 
     new Facade().post('/api/messages', message, (response) => {
-      console.log(response)
+      setData([...data, message])
+      message.timestamp = message.timestamp.toLocaleString();
+      scrollLastMessageIntoView(inputRef)
     }, (error) => {
       console.log(error)
     })
-    // send to database
-
-    scrollLastMessageIntoView()
-    message.timestamp = message.timestamp.toLocaleString();
-    setData([...data, message])
   }
 
   useEffect(() => {
       fetchMessages(room);
-      scrollLastMessageIntoView()
-
-      return () => {
-        clearTimeout(timeout)
-      }
+      return () => clearTimeout(timeout);
   }, [fetchMessages, room, timeout]);
 
   return (
@@ -72,13 +62,13 @@ export default function MessageRoom() {
       {/* <div> */}
       {loading ? <LoadingIndicator/> : null}
       {error ? <Redirect to="/messages"/> : null }
-      <div style={{minHeight: "calc(90vh - 5rem)"}}>
+      <div style={{minHeight: "calc(90vh - 5rem)", overflow: "scroll"}}>
       {data.map((data, index) => (
         <Message key={index} props={data} />
       ))}
       </div>
       {/* </div> */}
-      <div className="message-form" style={{paddingBottom: "2vh"}}>
+      <div className="message-form" ref={inputRef} style={{paddingBottom: "2vh"}}>
         <input className="message-input" value={input} onChange={(e) => setInput(e.target.value)}/>
         <button
           className="send-message"
