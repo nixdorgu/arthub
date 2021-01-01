5c6df06af3dd3f8724efc779338a589e21d9387e
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-01-01 22:05:38 +0800

    refactor: utilize user flow utility and profile header comp to abstract rendering details from comp

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -3,9 +3,8 @@ import Snackbar from '../Snackbar';
 import {useAuth} from "../../context/AuthContext";
 import Facade from '../../utils/Facade';
 import CommissionModal from '../modals/CommissionModal';
-import LoadingIndicator from '../LoadingIndicator';
-import ProfileInteractions from './ProfileInteractions';
-import showFullName from '../../tests/showFullName';
+import UserFlow from '../../utils/UserFlow';
+import ProfileHeader from './ProfileHeader';
 
 function Profile({match}) { 
     const [loading, setLoading] = useState(true);
@@ -93,27 +92,15 @@ function Profile({match}) {
         <div style={{flexDirection: "column", display: "flex", alignItems:"center", width: "100%"}}>
         {<Snackbar hidden={showSnackbar} props={{ message: snackbarMessage, snackbarRef, error: true, showSnackbar, setShowSnackbar}}/> }
 
-        {loading ?  <LoadingIndicator/> : (
-            // TODO: refactor this
-            <div className="profile-proper" style={{width: "80%"}}>
-                <div className="profile header" style={{display: "flex", justifyContent: "center", alignItems: "center", height: "max-content", width: "100%"}}>
-                    <div className="image" style={{marginRight: "3vw"}}>
-                        <img src={src} style={imgStyle} alt="profile"/>
-                    </div>
-                    <div className="bio" style={{display: "flex", flexDirection: "column", justifyContent: "flex-end", height: "100%", width: "100%"}}>
-                        <p>{showFullName(profileData)}</p>
-                        <i style={{padding: ".5rem 0", fontSize: "1.5vh"}}>{`Member since ${new Date(profileData['member_since']).toLocaleDateString()}`}</i>
-                        <ProfileInteractions props={{isMe, user, setShowHireModal, profileData}}/>
-
-                    </div>
-                </div>
+        <UserFlow isLoading={loading} success={<div className="profile-proper" style={{width: "80%"}}>
+                <ProfileHeader isMe={isMe} user={user} setShowHireModal={setShowHireModal} profileData={profileData} src={src} imgStyle={imgStyle}/>
                 {/* modal should close when clicked anywhere else same with hamburger */}
                 {!isMe && profileData['user_classification'] === 'artist' && (<CommissionModal show={showHireModal} handleClose={(e) => setShowHireModal(false)} handleSubmit={processTransaction}/>)}
                 <div style={{lineBreak: "normal", wordBreak: "break-word"}}>
                 {JSON.stringify(profileData)}
                 </div>
             </div>
-        )}
+        }/>
             {/* <div className="bottom-portion" style={{width:"100%", marginTop:"3vh"}}>
                 <div id="options" className="row" style={{background: "blue", padding: "1rem", marginBottom: "1vh"}}>
                     <span>Reviews</span>
