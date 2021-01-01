ffec76372d9a3f664c9530d5545359130550cd0a
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-01-01 20:13:37 +0800

    feat: add event handlers to close modal on click away

--- client/src/components/modals/Modal.js
+++ client/src/components/modals/Modal.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, {useEffect} from "react";
 import './modal.css';
 
 export default function Modal({ handleClose, handleSubmit, header, show, children, btnGroup }) {
   const className = show ? "modal-body show" : "modal-body hide";
   const buttonGroup = (<><button className="modal button-group cancel" onClick={handleClose}>Cancel</button>
         <button form="modal-form" type="submit" className="modal button-group submit" onClick={handleClose}>Submit</button></>);
+
+    useEffect(() => {
+        function removeListener() {
+            return window.removeEventListener('click', onClick, true);
+        }
+
+        function onClick(e, element = '.modal-body') {
+            const modal = document.querySelector(element);
+
+            if (!e.path.includes(modal) && show) {
+                handleClose();
+                removeListener();
+            }
+        }
+
+        if (show) {
+            window.addEventListener('click', onClick, true);
+        }
+
+        return () => removeListener();
+    });
+
   return (
     <div className={className}>
         <section>
