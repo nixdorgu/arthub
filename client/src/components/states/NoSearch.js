49be20c7c87851edb8fc92f02e353ff7a6b476c9
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-01-03 00:14:05 +0800

    feat: add component for no artists found case

--- client/src/components/states/NoSearch.js
+++ client/src/components/states/NoSearch.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import clumsy from "../../images/clumsy.svg";
 
 export default function NoSearch() {
     return (
-        <div style={{height: "100vh", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
-            No artists found!
-        </div>
+        <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "space-evenly",
+          height: "100%",
+          padding: "2rem"
+        }}
+      >
+          <div>
+          <h1 style={{textAlign: "center"}}>No Artists Found</h1>
+          </div>
+        <img
+          src={clumsy}
+          alt="Serenity"
+          style={{
+            width: "100%",
+            display: "block",
+            maxHeight: "50vh",
+            margin: "2rem 0",
+          }}
+        />
+        <i style={{ textAlign: "center" }}>
+          "We couldn't find artists, want to try a different keyword?"
+        </i>
+      </div>
     )
 }
