5a24b75d5c17df804ada19ea49022b00a350953d
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-01-03 00:16:32 +0800

    refactor: use user flow utility to consolidate logic in home component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,8 +3,9 @@ import { Redirect } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
 import Facade from '../utils/Facade';
 import ArtistCard from './ArtistCard';
-import LoadingIndicator from './LoadingIndicator';
 import UserFlow from '../utils/UserFlow';
+import NoSearch from './states/NoSearch';
+import Error500 from './states/Error500';
 
 function Home() {
   const ctx = useContext(AuthContext);
@@ -15,12 +16,8 @@ function Home() {
 
   function searchArtists(input) {
     new Facade().get(`/api/artists/${input}`, (success) => {
-      if (success.length > 0) {
-        setError(false);
-        setData(() => success);
-      } else {
-        setError(true);
-      }
+      setData(() => success);
+      success.length > 0 ? setError(false) : setError(true);
     }, (error) => {
       setError(true);
     });
@@ -31,29 +28,25 @@ function Home() {
     setLoading(false);
   }, [input]); 
 
-  // ADD ERROR, LOADING, NO DATA STATES
-    return (
-      <div style={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", flexWrap: "wrap"}}>
-        {/* {ctx.authenticated ? null : <Redirect to="/login"/>} */}
-        {/* don't forget genres */}
-        <form style={{display: "flex", width: "80%", maxWidth: "600px"}} onSubmit={(e) => {
-          e.preventDefault();
-          searchArtists(input)
-          }}>
-            <input type="text" placeholder="Search for artists, genres, etc" style={{flex: "1", fontFamily: "Montserrat, sans-serif", padding: ".35rem"}} onChange={(e) => setInput(e.target.value)} value={input}></input>
-            <button type="submit" className="search" style={{background: "#ff5678", padding: ".5rem", fontFamily: "Montserrat"}}>Search</button>
-        </form>
-        <>
-        {data.length > 0 ? (
-          <div id="artists" style={{width: "calc(100vw - 8rem)", margin: "2rem", display: "grid", justifyContent: "center", gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))", gridTemplateRows: "auto", gap: ".5rem"}}>
-          {data.map(artist => <ArtistCard key={artist.user_id} props={{artist}}/>)}
-          </div>
-        ) : (
-          <LoadingIndicator/>
-        )}
-        </> 
+  return (
+    <div style={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", flexWrap: "wrap"}}>
+      {/* don't forget genres */}
+      <form style={{display: "flex", width: "80%", maxWidth: "600px"}} onSubmit={(e) => {
+        e.preventDefault();
+        searchArtists(input)
+        }}>
+          <input type="text" placeholder="Search for artists, genres, etc" style={{flex: "1", fontFamily: "Montserrat, sans-serif", padding: ".35rem"}} onChange={(e) => setInput(e.target.value)} value={input}></input>
+          <button type="submit" className="search" style={{background: "#ff5678", padding: ".5rem", fontFamily: "Montserrat"}}>Search</button>
+      </form>
+      <UserFlow isLoading={loading} isError={error} error={
+        data.length === 0 ? <NoSearch/> : <Error500/>
+      } success={
+        <div id="artists" style={{width: "calc(100vw - 8rem)", margin: "2rem", display: "grid", justifyContent: "center", gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))", gridTemplateRows: "auto", gap: ".5rem"}}>
+        {data.map(artist => <ArtistCard key={artist.user_id} props={{artist}}/>)}
         </div>
-    );
+      }/>
+      </div>
+  );
 }
 
 export default Home;
