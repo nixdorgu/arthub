eb1f8b86e80ece3cd460063695a517c0c254de3b
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-01-03 00:10:05 +0800

    feat: add loading and error state to component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,27 +4,37 @@ import { AuthContext } from '../context/AuthContext';
 import Facade from '../utils/Facade';
 import ArtistCard from './ArtistCard';
 import LoadingIndicator from './LoadingIndicator';
+import UserFlow from '../utils/UserFlow';
 
 function Home() {
   const ctx = useContext(AuthContext);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
   const [data, setData] = useState([]);
   const [input, setInput] = useState('');
 
   function searchArtists(input) {
     new Facade().get(`/api/artists/${input}`, (success) => {
-      setData(success)
+      if (success.length > 0) {
+        setError(false);
+        setData(() => success);
+      } else {
+        setError(true);
+      }
     }, (error) => {
-      console.log(error)
-    })
+      setError(true);
+    });
   }
 
   useEffect(() => {
     searchArtists(input);
+    setLoading(false);
   }, [input]); 
 
+  // ADD ERROR, LOADING, NO DATA STATES
     return (
       <div style={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", flexWrap: "wrap"}}>
-        {ctx.authenticated ? null : <Redirect to="/login"/>}
+        {/* {ctx.authenticated ? null : <Redirect to="/login"/>} */}
         {/* don't forget genres */}
         <form style={{display: "flex", width: "80%", maxWidth: "600px"}} onSubmit={(e) => {
           e.preventDefault();
@@ -35,7 +45,7 @@ function Home() {
         </form>
         <>
         {data.length > 0 ? (
-          <div id="artists" style={{width: "calc(100vw - 4rem)", margin: "2rem", display: "grid", gridTemplateColumns: "repeat(auto-fit, minmax(250px, 1fr))", gridTemplateRows: "auto", gap: ".5rem"}}>
+          <div id="artists" style={{width: "calc(100vw - 8rem)", margin: "2rem", display: "grid", justifyContent: "center", gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))", gridTemplateRows: "auto", gap: ".5rem"}}>
           {data.map(artist => <ArtistCard key={artist.user_id} props={{artist}}/>)}
           </div>
         ) : (
