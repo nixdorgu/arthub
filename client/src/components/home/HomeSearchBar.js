4960572f03cca16d63b066139ebbc6fdf96a875e
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-01-03 00:29:40 +0800

    feat: create component for home search bar

---
+++ client/src/components/home/HomeSearchBar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function HomeSearchBar({onSubmit, input, setInput}) {
+    return (
+        <form style={{display: "flex", width: "80%", maxWidth: "600px"}} onSubmit={(e) => onSubmit(e)}>
+              <input type="text" placeholder="Search for artists, genres, etc" style={{flex: "1", fontFamily: "Montserrat, sans-serif", padding: ".35rem"}} onChange={(e) => setInput(e.target.value)} value={input}></input>
+              <button type="submit" className="search" style={{background: "#ff5678", padding: ".5rem", fontFamily: "Montserrat"}}>Search</button>
+          </form>
+    )
+}
