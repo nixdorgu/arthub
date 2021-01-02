288695ca0634ac9f99eb1c67504e5754d4f377fc
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-01-03 00:35:14 +0800

    refactor: add input as a parameter of onSubmit function

--- client/src/components/home/HomeSearchBar.js
+++ client/src/components/home/HomeSearchBar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function HomeSearchBar({onSubmit, input, setInput}) {
     return (
-        <form style={{display: "flex", width: "80%", maxWidth: "600px"}} onSubmit={(e) => onSubmit(e)}>
+        <form style={{display: "flex", width: "80%", maxWidth: "600px"}} onSubmit={(e) => onSubmit(e, input)}>
               <input type="text" placeholder="Search for artists, genres, etc" style={{flex: "1", fontFamily: "Montserrat, sans-serif", padding: ".35rem"}} onChange={(e) => setInput(e.target.value)} value={input}></input>
               <button type="submit" className="search" style={{background: "#ff5678", padding: ".5rem", fontFamily: "Montserrat"}}>Search</button>
           </form>
