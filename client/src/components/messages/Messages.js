e88beee91d64500872b3fcd2136e9a63706daf05
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-01-03 01:15:11 +0800

    feat: refactor componenet with user flow util and add empty state

--- client/src/components/messages/Messages.js
+++ client/src/components/messages/Messages.js
@@ -1,30 +1,42 @@
 import React, { useEffect, useState, useCallback } from "react";
 import { useAuth } from "../../context/AuthContext";
+import { useSocket } from "../../context/SocketContext";
 import Facade from "../../utils/Facade";
 import NoMessages from "../states/NoMessages";
-import LoadingIndicator from "../LoadingIndicator";
 import MessageCard from "./MessageCard";
+import UserFlow from "../../utils/UserFlow";
+import Error500 from "../states/Error500";
 
 export default function Messages() {
   let timeout;
   const [rooms, setRooms] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
+  const [empty, setEmpty] = useState(false);
   const {user} = useAuth();
+  const socket = useSocket();
 
   const getRooms = useCallback(() => {
     new Facade().get(
       `/api/messages/${user.id}`,
       (response) => {
-        setError(false);
+        const isEmpty = response.length === 0;
+
         setRooms(response);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-        timeout = setTimeout(() => getRooms(user.id), 10000);
+        setError(isEmpty);
+        setEmpty(isEmpty);
+        // ADD ON_NEW_ROOM EVENT
+        // CONSIDER USING USER_ID AS LISTENER ON NEW MESSAGE
+
+        // rooms.map(room => {
+        //   const id = room.room_id;
+        //   return socket.emit('join', id);
+        // })
       },
       (error) => {
         // show error page
+        setEmpty(false);
         setError(true);
-        console.log(error);
       }
     );
 
@@ -36,20 +48,20 @@ export default function Messages() {
       getRooms();
     }
     return () => clearTimeout(timeout);
-  }, [getRooms, timeout, user]); // having rooms here makes it render exponentially
+  }, [getRooms, user, timeout]); // having rooms here makes it render exponentially
 
   return (
     <div className="messages">
-      {loading ? <LoadingIndicator /> : null}
-      {rooms.length > 0 && !error ? (
-        rooms.map((data, index) => (
+      <UserFlow isLoading={loading} isError={error} error={
+        empty ? <NoMessages/> : <Error500/>
+      }
+      success={
+        <>
+       {rooms.map((data, index) => (
           <MessageCard key={index} props={{ data, index, user }} />
-        ))
-      ) : error ? (
-        <div>Something went wrong!</div>
-      ) : (
-        <NoMessages />
-      )}
+        ))}
+        </>
+      }/>
     </div>
   );
 }
