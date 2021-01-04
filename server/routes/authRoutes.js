391d61dc8d225ccda91d515d3947accd3f210578
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-01-04 17:56:38 +0800

    refactor: move login and register routes to auth

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -8,6 +8,106 @@ const formatPayload = require('../src/formatPayload');
 const authRoute = () => {
   const router = express.Router();
 
+  router.post(
+    '/login',
+    middleware.isNotAuthenticated,
+    passport.authenticate('local', { session: false }),
+    (req, res) => {
+      let { user } = req;
+
+      if (JSON.stringify(user) === JSON.stringify({})) {
+        return res
+          .status(401)
+          .json({ success: false, message: 'Incorrect credentials.' });
+      }
+
+      user = formatPayload(user);
+
+      return req.login(user, { session: false }, (error) => {
+        if (error) {
+          return res
+            .status(500)
+            .json({ success: false, message: 'Something went wrong' });
+        }
+        return jwt.sign(
+          user,
+          process.env.JWT_SECRET,
+          { expiresIn: '1d' },
+          (err, token) => {
+            if (err) {
+              return res
+                .status(500)
+                .json({ success: false, message: 'Something went wrong' });
+            }
+            return res.status(200).json({ token });
+          },
+        );
+      });
+    },
+  );
+
+  router.post('/register', (req, res) => {
+    const {
+      firstName, lastName, email, password, isArtist,
+    } = req.body;
+
+    bcrypt.hash(password, 12, (hashError, hashedPassword) => {
+      if (hashError) {
+        return res
+          .status(500)
+          .json({ success: 'false', message: 'Something went wrong.' });
+      }
+
+      return client.query(
+        'SELECT email FROM users WHERE email = $1',
+        [email],
+        (error, duplicate) => {
+          if (error || duplicate.rows.length !== 0) {
+            return res
+              .status(409)
+              .json({ success: 'false', message: 'Email already in use.' });
+          }
+
+          const userClassification = isArtist ? 'artist' : 'customer';
+
+          const onSuccessfulRegistration = (response) => response.status(200).json({ success: true, message: 'Account created successfully.' });
+
+          const createReferenceToArtist = (response, user) => {
+            const id = user.user_id;
+            return client.query(
+              'INSERT INTO artists (artist_id) VALUES ($1)',
+              [id],
+              // eslint-disable-next-line no-unused-vars
+              (artistError, artistResult) => {
+                if (artistError) return response.status(500).json({ success: false });
+                return onSuccessfulRegistration(response);
+              },
+            );
+          };
+
+          const onError = (response, dbError, result) => {
+            if (dbError) {
+              return response
+                .status(500)
+                .json({ success: false, message: 'Something went wrong.' });
+            }
+
+            const user = result.rows[0];
+            return user.user_classification !== 'artist'
+              ? onSuccessfulRegistration(res)
+              : createReferenceToArtist(res, user);
+          };
+
+          return client.query(
+            'INSERT INTO users VALUES (DEFAULT, $1, $2, $3, $4, DEFAULT, DEFAULT, $5) RETURNING *',
+            [firstName, lastName, email, hashedPassword, userClassification],
+            (dbError, result) => onError(res, dbError, result),
+          );
+        },
+      );
+    });
+  });
+
   router.get(
     '/facebook', middleware.isNotAuthenticated,
     passport.authenticate('facebook', { session: false, scope: ['email'] }),
