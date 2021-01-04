f05d88c7e3fd2966b605b7033298275dd2b8ba97
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-01-05 02:49:07 +0800

    feat: add profile image table modification migration

---
+++ server/migrations/20210105024342_modify_profile_image_table.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex) {
+  return knex.raw(`
+    ALTER TABLE profile_images DROP COLUMN type, DROP COLUMN image;
+    ALTER TABLE profile_images ADD COLUMN link VARCHAR;
+  `);
+};
+
+exports.down = function(knex) {
+  return knex.raw(`
+    ALTER TABLE profile_images DROP COLUMN link;
+    ALTER TABLE profile_images ADD COLUMN type VARCHAR(11), ADD COLUMN image BYTEA;
+  `);
+};
