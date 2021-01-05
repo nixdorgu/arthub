b20d093b30dd6b927f9d2d74086b979ca6366152
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-01-05 14:29:17 +0800

    refactor: refactor class-based Facade to functional fetch

--- client/src/utils/Facade.js
+++ client/src/utils/Facade.js
@@ -1,5 +1,37 @@
 import { getToken } from "./Tokens";
 
+export function fetch(url, { method, image, data, success, error } = {}) {
+  const xhr = new XMLHttpRequest();
+  const token = getToken();
+
+  const body = {...data, ...image};
+
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4) {
+      const xhrResponseText = xhr.responseText;
+      let response;
+
+      try {
+        response = JSON.parse(xhrResponseText);
+      } catch {
+        response = { message: 'Something went wrong' }
+      }
+      
+      if (xhr.status === 200) {
+        return success(response);
+      } else {
+        return error(response);
+      }
+    }
+  };
+
+  
+  xhr.open(method.toUpperCase(), url);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.setRequestHeader("Authorization", `Bearer ${token}`);
+  xhr.send(JSON.stringify(body));
+}
+
 export default class Facade {
     post(url, data, success, error) {
       const xhr = new XMLHttpRequest();
