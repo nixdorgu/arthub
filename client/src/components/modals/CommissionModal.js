fa3e64497da8a89d920db74b14cb0b82eced3ae4
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-01-07 21:03:34 +0800

    revert: make all inputs required

--- client/src/components/modals/CommissionModal.js
+++ client/src/components/modals/CommissionModal.js
@@ -4,10 +4,10 @@ import Modal from './Modal';
 export default function CommissionModal({show, handleClose, handleSubmit}) {
     return (
         <Modal show={show} handleClose={handleClose} header={'Commission Form'} handleSubmit={handleSubmit}>
-            <input type="text" aria-label="title" placeholder="Title (maximum of 30 characters)" min="1" max="30" required/>
+            <input type="text" aria-label="title" placeholder="Title (maximum of 30 characters)" min="1" max="30" required />
             <textarea className="short-description" aria-label="short description" placeholder="Short description (maximum of 80 characters)" min="1" max="80" required/>
             <textarea className="long-description" aria-label="long description" placeholder="Long description (maximum of 300 characters)" min="1" max="300" required/>
-            <input id="price" name="price" type="number" min="1.00" step="any" aria-label="proposed price" placeholder="Proposed price"/>
+            <input id="price" type="number" aria-label="proposed price" placeholder="Proposed price" required />
             <p className="price-disclaimer">* We operate using USD</p>
         </Modal>
     )
