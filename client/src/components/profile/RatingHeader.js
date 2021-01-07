27b767287dafab2b194c973167bf5238ee8a3bb9
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-01-08 04:38:47 +0800

    feat: add rating header component

---
+++ client/src/components/profile/RatingHeader.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function RatingHeader() {
+    return (
+        <div>
+          <h1 className="favorite-header">User Rating:</h1>
+          <span className="rating-span">* Based on success rate of previous transactions on this platform. </span>
+        </div>
+    )
+}
