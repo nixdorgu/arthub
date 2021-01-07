ce264a32a492d4d34ef10a3cd58be9be49aef031
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-01-08 04:45:09 +0800

    feat: add favorite component

---
+++ client/src/components/profile/Favorite.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Image from './Image'
+
+export default function Favorite({src, name}) {
+    return (
+        <div className="favorite-container">
+            <Image alt={name } src={src}/>
+            <p className="favorite-p">{name || ''}</p>
+        </div>
+    )
+}
