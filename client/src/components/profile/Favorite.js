7568edc2c9899a191d7958a1f64f2271cd342cee
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-01-08 05:21:54 +0800

    refactor: update favorite component

--- client/src/components/profile/Favorite.js
+++ client/src/components/profile/Favorite.js
@@ -4,7 +4,7 @@ import Image from './Image'
 export default function Favorite({src, name}) {
     return (
         <div className="favorite-container">
-            <Image alt={name } src={src}/>
+            <Image alt={name} src={src}/>
             <p className="favorite-p">{name || ''}</p>
         </div>
     )
