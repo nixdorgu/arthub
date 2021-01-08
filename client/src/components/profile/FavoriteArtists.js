b8d55f64098d495abdbcb8c0396d7310ab35552a
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-01-08 15:45:58 +0800

    refactor: change h1 tag to h3 in favorite artists component

--- client/src/components/profile/FavoriteArtists.js
+++ client/src/components/profile/FavoriteArtists.js
@@ -4,7 +4,7 @@ import Favorite from "./Favorite";
 export default function FavoriteArtists({ data = [] }) {
   return data.length > 0 ? (
     <div className="container">
-      <h1 className="favorite-header">Favorite Artists: </h1>
+      <h3 className="favorite-header">Favorite Artists: </h3>
       <div className="profile-favorite-container">
         {data.map((item, index) => (
           <Favorite key={index} src={item.image} name={item.name} />
